8dfc5551be1de6774d518507a22566674527c369
LifeOfAitor <aitorgaillard99@gmail.com>
2024-08-04 19:53:53 +0200

    add lines

--- main.py
+++ main.py
@@ -10,9 +10,12 @@ df = pd.read_csv("topics.csv")
 pagecount = 0
 for index, row in df.iterrows():
     pdf.add_page()
+    # set header
     pdf.set_font(family="Times", style="B", size=12)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="l", ln=1)
-    pdf.line(10,21,200,21)
+    # add lines
+    for i in range(21, 290, 10):
+        pdf.line(10, i, 200, i)
 
     # footer - add 265 breaklines
     pdf.ln(265)
@@ -21,12 +24,17 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=5, txt=f"{pagecount + 1}", align="C")
     pagecount+=1
 
+    # add extra pages
     for i in range(row["Pages"]-1):
         pdf.add_page()
+        # add lines
+        for i in range(21, 290, 10):
+            pdf.line(10, i, 200, i)
         # footer - add 277 breaklines
         pdf.ln(277)
         pdf.set_font(family="Times", style="I", size=8)
         pdf.cell(w=0, h=5, txt=row["Topic"], align="C", ln=1)
         pdf.cell(w=0, h=5, txt=f"{pagecount + 1}", align="C")
         pagecount += 1
+
 pdf.output("output.pdf")
